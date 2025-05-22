717b8500cd1411fb387baecc9b6302d05ff6efe0
El Houcine Qara <elhoucine.qara@harx.ai>
2025-05-22 14:31:33 +0100

    Add qiankun config

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,11 @@ export default defineConfig(({ mode }) => {
       port: 5183,
       cors: true,
       hmr: false,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+      },
       fs: {
         strict: true, // Ensure static assets are correctly resolved
       },
